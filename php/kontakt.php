2580da38d134d8bd8837933204a47589f9342f24
Elyesa Duru <elydur@icloud.com>
2024-03-07 10:28:26 +0100

    Elements added and EMail-fixxed and then destroyed again(2Faktor AUthentifiktaion und APP-Passwort!)

--- php/kontakt.php
+++ php/kontakt.php
@@ -1,4 +1,11 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer\src\Exception.php';
+require 'PHPMailer\src\PHPMailer.php';
+require 'PHPMailer\src\SMTP.php';
+
 // Überprüfen, ob das Formular abgesendet wurde
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Formulardaten erfassen
@@ -6,19 +13,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    // Hier können weitere Aktionen ausgeführt werden, z.B. E-Mails senden, Daten speichern usw.
-    
     // Beispiel: E-Mail senden
-    $to = "jon.dieroff.23@lehre.mosbach.dhbw.de";
-    $subject = "Kontaktformular Nachricht";
-    $body = "Name: $name\n";
-    $body .= "E-Mail: $email\n";
-    $body .= "Nachricht:\n$message";
-    mail($to, $subject, $body);
+    $mail = new PHPMailer(true);
+    try {
+        //Server-Einstellungen
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com'; // SMTP-Server
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'gruppehtml@gmail.com'; // SMTP-Benutzername
+        $mail->Password   = 'bgxm ymai uiod hitm'; // SMTP-Passwort
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587; // Port des SMTP-Servers
 
-    // Bestätigung an den Benutzer senden
-    echo "Vielen Dank für Ihre Nachricht, $name! Wir werden uns bald bei Ihnen melden.";
+        // Empfänger
+        $mail->setFrom('from@example.com', $name);
+        $mail->addAddress('gruppehtml@gmail.com','HTMLgruppe');
+
+        // Inhalt der E-Mail
+        $mail->isHTML(false);
+        $mail->Subject = 'Kontaktformular Nachricht';
+        $mail->Body    = "Name: $name\nE-Mail: $email\nNachricht:\n$message";
+
+        $mail->send();
+        echo "Vielen Dank für Ihre Nachricht, $name! Wir werden uns bald bei Ihnen melden.";
+    } catch (Exception $e) {
+        echo "Es ist ein Fehler aufgetreten: {$mail->ErrorInfo}";
+    }
 } else {
     // Wenn das Formular nicht abgesendet wurde, Weiterleitung oder Fehlermeldung anzeigen
     echo "Es ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.";
 }
+?>
