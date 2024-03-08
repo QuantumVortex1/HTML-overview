91c7e1ec5e50dfcf51daf6394068e33bafa3443d
Jonas Dieroff <152855101+QuantumVortex1@users.noreply.github.com>
2024-03-08 15:36:12 +0100

    minor changes

--- php/templates/footer.php
+++ php/templates/footer.php
@@ -1,4 +1,5 @@
 <div>
+  <?php echo $_SERVER['DOCUMENT_ROOT']; ?>
   <?php include($_SERVER['DOCUMENT_ROOT'].'/HTML-overview/php/impressum.php'); ?>
   <?php include($_SERVER['DOCUMENT_ROOT'].'/HTML-overview/php/datenschutz.php'); ?>
 </div>
